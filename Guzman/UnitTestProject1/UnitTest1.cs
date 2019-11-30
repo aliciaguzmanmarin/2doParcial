160161523fd35c647227f182bd77ec3eb9f77983
visual_community@outlook.com <visual_community@outlook.com>
2019-11-30 12:45:35 -0400

    librerias de clase

---
+++ Guzman/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Julia;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SaveTest()
+        {
+            //organizar 
+            Company compañia = new Company("alicia", 110.15);
+            double ve = 125.15;
+
+            //actuar 
+            compañia.Save(15);
+            double vo = compañia.SaveProject;
+
+            //comprobar 
+            Assert.AreEqual(ve, vo);
+        }
+
+        [TestMethod]
+        public void SpendTest()
+        {
+            //organizar 
+            Company compañia = new Company("alicia", 140.15);
+            double ve = 125.15;
+
+            //actuar 
+            compañia.Spend(15);
+            double vo = compañia.SaveProject;
+
+            //comprobar 
+            Assert.AreEqual(ve, vo);
+        }
+
+
+        [TestMethod]
+        public void ChangeNameTest()
+        {  
+            //organizar 
+            Company compañia = new Company("julia", 110.15);
+            string ve = "aliciaguzman";
+
+            //actuar 
+            compañia.ChangeName("aliciaguzman");
+            string vo = compañia.Name;
+
+            //comprobar 
+            Assert.AreEqual(ve, vo);
+
+        }
+    }
+}
+
